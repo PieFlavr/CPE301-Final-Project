01c647b49eac4ad7bc718ed4a7ca86a2a6171e76
PieFlavr <lucasadampinto@gmail.com>
2024-12-14 18:13:46 -0800

    Set up temporary LED code and circuitry.

--- CPE301_FinalProject/CPE301_FinalProject.ino
+++ CPE301_FinalProject/CPE301_FinalProject.ino
@@ -5,9 +5,13 @@
 //LCD additionallu uses a 330 OHM resistor
 #include <LiquidCrystal.h>
 //const int RS = 11, EN = 12, D4 = 2, D5 = 3, D6 = 4, D7 = 5;
+
 const int RS = 39, EN = 41, D4 = 31, D5 = 33, D6 = 35, D7 = 37; 
 LiquidCrystal lcd(RS, EN, D4, D5, D6, D7);
 
+//LED System
+//temporary pins, replace with register macros later
+const int LED_R = 30, LED_Y = 32, LED_G = 34, LED_B = 36;
 
 void setup(){
     lcd.begin(16,2);
@@ -15,11 +19,21 @@ void setup(){
     
     //Testing Display
     lcd.write("Hello World!");
-    
+
+    //Temporary LED Pin Testing
+    pinMode(LED_R, OUTPUT);
+    pinMode(LED_Y, OUTPUT);
+    pinMode(LED_G, OUTPUT);
+    pinMode(LED_B, OUTPUT);
+
+    digitalWrite(LED_R, HIGH);
+    digitalWrite(LED_Y, HIGH);
+    digitalWrite(LED_G, HIGH);
+    digitalWrite(LED_B, HIGH);
 }
 
 void loop(){
-    
+
 }
 
 ISR(TIMER1_OVF_vect)
