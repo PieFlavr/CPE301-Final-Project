faeb3f87fca23cb241b49357808fdec4215048a7
PieFlavr <lucasadampinto@gmail.com>
2024-12-15 20:44:54 -0800

    Implemented reset button interrupt basics and UART functions.

--- CPE301_FinalProject/CPE301_FinalProject.ino
+++ CPE301_FinalProject/CPE301_FinalProject.ino
@@ -41,7 +41,7 @@ const int fanSpeedPIN = 12, fanDIR1 = 11, fanDIR2 = 13, fanSpeed = 90;
  * DHT11 Water Sensor !!!NEED DOCUMENTATION/REFERENCE SHEET NEEDED!!!
  */
 #include <dht.h>
-const int DHT11_PIN = 54; //Pin A0
+const int DHT11_PIN = 54; //Analog Pin A0
 volatile unsigned char* PORT_G = (unsigned char*) 0x34; //WRITE adddres, 0x14 is READ!!!
 volatile unsigned char* DDR_G = (unsigned char*) 0x33; //WRITE address, 0x13 is READ!!!
 dht DHT_Sensor;
@@ -65,22 +65,49 @@ volatile unsigned char* my_ADCSRA = (unsigned char*) 0x7A;   // ADC Control and 
 volatile unsigned int* my_ADC_DATA = (unsigned int*) 0x78;   // ADC Data Register
 
 /**
+ * Interrupt/Reset Button !!!NEED DOCUMENTATION/REFERENCE SHEET NEEDED!!!
+ */
+volatile unsigned char* DDR_D = (unsigned char*) 0x2A;  // PWM PIN 2
+volatile unsigned char* PORT_D = (unsigned char*) 0x2B;
+const int resetPin = 2;
+volatile int resetti = 0;
+
+/**
+ * UART Definitions !!!NEED DOCUMENTATION/REFERENCE SHEET NEEDED!!!
+ */
+#define RDA 0x80  
+#define TBE 0x20  
+volatile unsigned char *myUCSR0A = (unsigned char *)0x00C0; // UART Status Register A
+volatile unsigned char *myUCSR0B = (unsigned char *)0x00C1; // UART Status Register B
+volatile unsigned char *myUCSR0C = (unsigned char *)0x00C2; // UART Status Register C
+volatile unsigned int  *myUBRR0  = (unsigned int *) 0x00C4; // UART Baud Rate Register
+volatile unsigned char *myUDR0   = (unsigned char *)0x00C6; // UART Data Register
+
+/**
  * Function Definitions
  */
 // Register Manipulators
 void writeRegister(unsigned char* address, int bit, int value);
+unsigned int adc_read(unsigned char adc_channel_num);
 unsigned int readRegister(unsigned char* address, int bit);
 
+//Interrupt Function
+void reset();
+
+//UART Functions
+void U0init(int U0baud);
+unsigned char U0kbhit();
+unsigned char U0getchar();
+void U0putchar();
+void UART_print(unsigned char* cstring);
+
 void setup(){
-    Serial.begin(9600);
+    U0init(9600); //Setting the UART Baud Rate
 
-    rtc.begin();
+    rtc.begin(); //Start the RTC object
 
-    display.begin(16,2);
+    display.begin(16,2); //Initialize and clear LCD
     display.clear();
-    
-    // Testing Display
-    display.write("Hello World!");
 
     // LED Testing (DIGITAL 30,32,34,36 respectively)
     writeRegister(DDR_C, 7, 1); //Set to OUTPUT
@@ -93,27 +120,41 @@ void setup(){
     writeRegister(PORT_C, 3, 0);
     writeRegister(PORT_C, 1, 0);
 
-    //Fan Motor Testing, !!!not sure if library is allowed ???!!!
+    // Fan Motor Testing, !!!not sure if library is allowed ???!!!
     pinMode(fanSpeedPIN, OUTPUT);
     pinMode(fanDIR1, OUTPUT);
     pinMode(fanDIR2, OUTPUT);
 
-    //Analog Read Setup
+    // Analog Read Setup
     ADC_setup();
 
-    //Water Level Sensor
+    // Water Level Sensor (ANALOG A1)
     writeRegister(DDR_G, 1, 1); //Set to OUTPUT
     writeRegister(PORT_G, 1, 0); //Set to OFF
 
-    //Vent Mottor Init
+    // Vent Motor Init
     ventMotor.setSpeed(5);
+
+    // Reset Button Setup
+    writeRegister(DDR_D,2,0); //Set as INPUT
+    writeRegister(PORT_D,2,1); //Set as PULL-UP
+    attachInterrupt(digitalPinToInterrupt(resetPin), reset, FALLING);
+
+    display.write("Hello World!"); // Hello World Message!
 }
 
 void loop(){
+    unsigned char value_buffer[50]; //Buffer to convert numbers to printable strings
+
     //Temperature Sensing
     int chk = DHT_Sensor.read11(DHT11_PIN);
     int temperature = DHT_Sensor.temperature; // Already at integer precision
     int humidity = DHT_Sensor.humidity; // So no information lost doing this
+
+    //Interrupt Testing
+    sprintf(value_buffer,"%d",resetti);
+    UART_print("The hell???:: ");
+    UART_println(value_buffer);
 
     //LCD Printout
     display.clear();
@@ -130,50 +171,50 @@ void loop(){
 
     //TEMPORARY WATER LEVEL TESTING
     writeRegister(PORT_G, 1, 1); //Set to ON
-    int rawWaterLevel = adc_read(1);
-    Serial.print("Water Level Raw Value:: ");
-    Serial.println(rawWaterLevel); //100 lower bound, 300 upper bound calibration
+
+    int rawWaterLevel = adc_read(1); //100 lower bound, 300 upper bound calibration
+
+    sprintf(value_buffer,"%d",rawWaterLevel);
+    UART_print("Water Level Raw Value:: ");
+    UART_println(value_buffer); 
+
     writeRegister(PORT_G, 1, 0); //Set to OFF
 
     //Fan Motor Testing
     digitalWrite(fanDIR1,LOW);
     digitalWrite(fanDIR2,HIGH);
-    analogWrite(fanSpeedPIN,0);
+    analogWrite(fanSpeedPIN,255);
 
     //Stepper Motor Testing
     int stepperControl = adc_read(2); //200 lower bound, 800 upper bound for "up" and "down"
-    Serial.print("Stepper Control Raw Value:: ");
-    Serial.println(stepperControl);
     if(stepperControl >= 800){
       ventMotor.step(100);
     } else if (stepperControl <= 200){
       ventMotor.step(-100);
     }
     
-
     //RTC Testing
-    DateTime now = rtc.now();
-    Serial.print(now.year(), DEC);
-    Serial.print('/');
-    Serial.print(now.month(), DEC);
-    Serial.print('/');
-    Serial.print(now.day(), DEC);
-    Serial.print(" (");
-    Serial.print(daysOfTheWeek[now.dayOfTheWeek()]);
-    Serial.print(") ");
-    Serial.print(now.hour(), DEC);
-    Serial.print(':');
-    Serial.print(now.minute(), DEC);
-    Serial.print(':');
-    Serial.print(now.second(), DEC);
-    Serial.println();
+    // DateTime now = rtc.now();
+    // Serial.print(now.year(), DEC);
+    // Serial.print('/');
+    // Serial.print(now.month(), DEC);
+    // Serial.print('/');
+    // Serial.print(now.day(), DEC);
+    // Serial.print(" (");
+    // Serial.print(daysOfTheWeek[now.dayOfTheWeek()]);
+    // Serial.print(") ");
+    // Serial.print(now.hour(), DEC);
+    // Serial.print(':');
+    // Serial.print(now.minute(), DEC);
+    // Serial.print(':');
+    // Serial.print(now.second(), DEC);
+    // Serial.println();
 
     delay(500);
 }
 
-ISR(TIMER1_OVF_vect)
-{
-
+void reset(){
+  resetti++;
 }
 
 /**
@@ -246,4 +287,78 @@ void writeRegister(unsigned char* address, int bit, int value) {
     } else {
         *address &= ~(1 << bit);  //Set the bit to 0 (LOW)
     }
+}
+
+/**
+ * @brief 
+ * 
+ * @param U0baud 
+ */
+void U0init(int U0baud)
+{
+ unsigned long FCPU = 16000000;           // CPU frequency
+ unsigned int tbaud = (FCPU / 16 / U0baud - 1); // Calculate baud rate
+
+ *myUCSR0A = 0x20; // Set UART Control and Status Register A
+ *myUCSR0B = 0x18; // Enable receiver and transmitter
+ *myUCSR0C = 0x06; // Set UART mode and data format
+ *myUBRR0  = tbaud; // Set calculated baud rate
+}
+
+/**
+ * @brief 
+ * 
+ * @return unsigned char 
+ */
+unsigned char U0kbhit()
+{
+  return *myUCSR0A & RDA; // Check RDA flag
+}
+
+/**
+ * @brief 
+ * 
+ * @return unsigned char 
+ */
+unsigned char U0getchar()
+{
+  return *myUDR0; // Return data from UART Data Register
+}
+
+/**
+ * @brief 
+ * 
+ * @param U0pdata 
+ */
+void U0putchar(unsigned char U0pdata)
+{
+  while((*myUCSR0A & TBE) == 0); // Wait until transmit buffer is empty
+  *myUDR0 = U0pdata;             // Load data into UART Data Register
+}
+
+/**
+ * @brief 
+ * 
+ * @param cstring 
+ */
+void UART_print(unsigned char* cstring){
+  int i = 0;
+  while(cstring[i] != '\0'){      // Loop until null terminator
+    U0putchar((unsigned char)(cstring[i])); // Transmit each character
+    i++;
+  }
+}
+
+/**
+ * @brief 
+ * 
+ * @param cstring 
+ */
+void UART_println(unsigned char* cstring){
+  int i = 0;
+  while(cstring[i] != '\0'){      // Loop until null terminator
+    U0putchar((unsigned char)(cstring[i])); // Transmit each character
+    i++;
+  }
+  U0putchar('\n');
 }
